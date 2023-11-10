011f380b6b13e48fc2ddbf47f6d711acd6d12965
ROGUEANOVI <ovidioromero66@gmail.com>
2023-11-09 21:03:25 -0500

    feat: Add repository and unit of work patterns

---
+++ BlogCore.DataAccess/Repository/GenericRepository.cs
@@ -0,0 +1,81 @@
+﻿using BlogCore.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BlogCore.DataAccess.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly DbContext _context;
+        internal DbSet<T> dbSet;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+            this.dbSet = context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties.Split(new char[]{ ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+
+            return query.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(int id)
+        {
+            T entityToRemove = dbSet.Find(id);
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+    }
+}
---
+++ BlogCore.DataAccess/Repository/IRepository/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace BlogCore.DataAccess.Repository.IRepository
+{
+    public interface IGenericRepository <T> where T : class
+    {
+        IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null);
+        
+        T GetById (int id);
+
+        T GetFirstOrDefault(Expression<Func< T, bool>> filter = null, string includeProperties = null);
+
+        void Add(T entity);
+
+        void Remove(int id);
+
+        void Remove(T entity);
+    }
+}
